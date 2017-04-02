2da1c95f0740f1aa50718f940cca015cd4770c0a
Cortlandt Winters <cort@cortwinters.net>
2017-04-02 08:57:41 -0400

    simplify structure and rename

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <TwoDToy></TwoDToy>
   </div>
 </template>
 
 <script>
-import TwoDToy from './components/TwoDToy'
 
 export default {
   name: 'app',
   components: {
-      TwoDToy
   }
 }
 </script>
